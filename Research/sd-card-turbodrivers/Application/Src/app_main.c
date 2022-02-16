20bbb2ea07bddde42e482c93336ee2070a1d1b6b
andrei1505k <andrei1505k@gmail.com>
2022-02-16 20:30:38 +0300

    убрал тесты

--- Research/sd-card-turbodrivers/Application/Src/app_main.c
+++ Research/sd-card-turbodrivers/Application/Src/app_main.c
@@ -2,14 +2,11 @@
 #include "fatfs.h"
 #include <stdio.h>
 
-#include "../../Library/lib-tsniimash/test.h"
-
 int app_main(void)
 {
 	FATFS fileSystem;
 	FIL testFile;
 	char testBuffer[16];
-	snprintf(testBuffer, sizeof(testBuffer), "good sum: %d", super_duper_sum(4, 3, 6));
 	UINT testBytes;
 	FRESULT res;
 	if(f_mount(&fileSystem, SDPath, 1) == FR_OK) {
@@ -17,6 +14,7 @@ int app_main(void)
 		path[12] = '\0';
 		res = f_open(&testFile, (char*)path, FA_WRITE | FA_CREATE_ALWAYS);
 		res = f_write(&testFile, (uint8_t*) testBuffer, sizeof(testBuffer), &testBytes);
+		res = f_read(&testFile, (uint8_t*) testBuffer, sizeof(testBuffer), &testBytes);
 		res = f_close(&testFile);
 	}
 	return 0;

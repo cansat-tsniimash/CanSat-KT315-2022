31fc0372fe85bd01862d3178f91fe5cf82975230
andrei1505k <andrei1505k@gmail.com>
2022-06-28 21:04:15 +0300

    radio timers

--- Code/main_project/Application/Src/timers.c
+++ Code/main_project/Application/Src/timers.c
@@ -3,6 +3,8 @@
 
 static uint32_t ds18b20_last_measurement = 0;
 static uint32_t sd_last_sync = 0;
+static uint32_t nrf_last_status_change = 0;
+static bool nrf_status = NRF_ON;
 
 void timer_update_ds18b20(void) {
 	ds18b20_last_measurement = HAL_GetTick();
@@ -14,8 +16,26 @@ void timer_update_sd(void) {
 	return;
 }
 
+static void timer_update_nrf(void) {
+	nrf_last_status_change = HAL_GetTick();
+	return;
+}
+
+static void status_update_nrf(void) {
+	if (nrf_status) {
+		if (HAL_GetTick() - nrf_last_status_change > NRF_DELAY_ON) {
+			nrf_status = NRF_OFF;
+		}
+	} else {
+		if (HAL_GetTick() - nrf_last_status_change > NRF_DELAY_OFF) {
+			nrf_status = NRF_ON;
+		}
+	}
+	timer_update_nrf();
+}
+
 bool timecheck_ds18b20(void) {
-	if (HAL_GetTick() - ds18b20_last_measurement < 750) {
+	if (HAL_GetTick() - ds18b20_last_measurement < DS18_DELAY) {
 		return false;
 	} else {
 		return true;
@@ -23,11 +43,16 @@ bool timecheck_ds18b20(void) {
 }
 
 bool timecheck_sd(void) {
-	if (HAL_GetTick() - sd_last_sync < 1000) {
+	if (HAL_GetTick() - sd_last_sync < SD_DELAY) {
 		return false;
 	} else {
 		return true;
 	}
+}
+
+bool timecheck_nrf(void) {
+	status_update_nrf();
+	return nrf_status;
 }
 
 bool timecheck_dosimeter(void) {
--- Code/main_project/Application/timers.h
+++ Code/main_project/Application/timers.h
@@ -3,10 +3,19 @@
 
 #include <includes.h>
 
+#define DS18_DELAY 750
+#define SD_DELAY 1000
+#define NRF_DELAY_ON 5000
+#define NRF_ON true
+#define NRF_DELAY_OFF 1000
+#define NRF_OFF false
+
 void timer_update_ds18b20(void);
 void timer_update_sd(void);
+
 bool timecheck_ds18b20(void);
 bool timecheck_sd(void);
+bool timecheck_nrf(void);
 bool timecheck_dosimeter(void);
 
 #endif /* TIMERS_H_ */
